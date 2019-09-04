a22832a9a86d39f4bda0622c2710e75ad7beb3b7
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-04 15:40:32 +0900

    Add props about list rendering

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/hello';
 
-const data1 = {
-  greet: 'Good Morning',
-  name: 'Takashi'
-};
-const data2 = {
-  greet: 'Good Evening',
-  name: 'Takako'
-};
+const greeting = ['Good Morning', 'Good Afternoon', 'Good Evening'];
 
 
 ReactDOM.render(
-  <div>
-    <Hello data = {data1} />,
-    <Hello data = {data2} />,
-  </div>,
+  <Hello data = {greeting} />,
   document.getElementById('root')
 );
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 const Hello = (props) => {
-    const { greet, name } = props.data; //分割代入
+    const greetings = props.data.map((greeting) => {
+        return <li>{greeting}</li>
+    });
     return (
-        // returnで返す要素は1くくりにしなければいけない
-        <div>
-            <p>{greet}, everyone!</p>
-            <p>My name is {name}.</p>
-        </div>
+        <ul>
+            {greetings}
+        </ul>
     )
 }
 export default Hello;
