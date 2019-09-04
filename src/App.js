33d59091ddc9bf099f6a573f3ebfc4dd8216bd99
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-04 15:24:27 +0900

    Add props feature2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/hello';
 
+const data1 = {
+  greet: 'Good Morning',
+  name: 'Takashi'
+};
+const data2 = {
+  greet: 'Good Evening',
+  name: 'Takako'
+};
+
+
 ReactDOM.render(
-  <Hello greet='Good Morning'/>,
+  <div>
+    <Hello data = {data1} />,
+    <Hello data = {data2} />,
+  </div>,
   document.getElementById('root')
 );
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
-const Hello = (props) => <div>{props.greet}, everyone!</div>
-
+const Hello = (props) => {
+    const { greet, name } = props.data; //分割代入
+    return (
+        // returnで返す要素は1くくりにしなければいけない
+        <div>
+            <p>{greet}, everyone!</p>
+            <p>My name is {name}.</p>
+        </div>
+    )
+}
 export default Hello;
